1489b2086a73d4f19dbe6b577380d4b8077e8c9a
iliailiaxxx <45950094+iliailiaxxx@users.noreply.github.com>
2022-12-28 17:40:14 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const express = require("express")
 const app = express()
-const axios = require('axios')
 const schedule = require('node-schedule')
 const PORT = process.env.PORT ||3000
-let ardiChat = '-799921306'
-let testChat = '-788676629'
-
+const ardiChat = '-799921306'
+const testChat = '-788676629'
+const array = ['Леша','Натали','Богдан','Настя','Илья','Женя','Коля','Влад','Саша Стребков','Саша Соколов','Яна','Даша','Костя','Кирилл','Саша Шкаликов']
+let counter = 0
+const text = ` сегодня дежурный
+- порядок на кухне 
+- кофемашина (чистота/молоко/слив) 
+- напоминаем коллегам, кто не убрал после себя
+- мусорные ведра`
+const TelegramBot = require('node-telegram-bot-api');
+const token = '5659999694:AAEKgoPjaUWZmHscyKLpsROpljHqRMSddoM';
+const bot = new TelegramBot(token);
 
 function sendRemind(){
-    axios.request(`https://api.telegram.org/bot5659999694:AAEKgoPjaUWZmHscyKLpsROpljHqRMSddoM/sendMessage?chat_id=${testChat}&text=SAYONARA BEACH!`)
+    if(counter === array.length){counter = 0}
+    bot.sendMessage(testChat,`${array[counter]}${text}`)
+    counter++
 }
 
-schedule.scheduleJob('60 * * * * 1-5', () => {sendRemind()})
+
+schedule.scheduleJob('5 * * * * *', () => {sendRemind()})
+
+// setInterval(()=>{
+//     sendRemind()
+// },15000)
 
 app.listen(PORT,()=>{})
