2c45e57521e202b6965a358919b28203bb4227fe
iliailiaxxx <45950094+iliailiaxxx@users.noreply.github.com>
2023-01-03 09:05:43 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,70 @@
 const express = require("express")
 const app = express()
 const schedule = require('node-schedule')
+const axios = require('axios')
 const PORT = process.env.PORT ||3000
 const ardiChat = '-799921306'
 const testChat = '2050460099'
 const array = ['Леша','Натали','Богдан','Настя','Илья','Женя','Коля','Влад','Саша Стребков','Саша Соколов','Яна','Даша','Костя','Кирилл','Саша Шкаликов']
-let counter = 0
 const text = ` сегодня дежурный
 - порядок на кухне 
 - кофемашина (чистота/молоко/слив) 
 - напоминаем коллегам, кто не убрал после себя
 - мусорные ведра`
+
+
 const TelegramBot = require('node-telegram-bot-api');
-const token = '5659999694:AAEKgoPjaUWZmHscyKLpsROpljHqRMSddoM';
+const token = '5659999694:AAHurTwbHAUZWKIwjYSXaE7DimmKZXNmZTY';
 const bot = new TelegramBot(token);
 
+let gdate = new Date()
 
 const rule = new schedule.RecurrenceRule();
-// rule.hour = 10;
-// rule.minute = 41;
-rule.second = 0;
+rule.hour = 14;
+rule.minute = 30;
+rule.second = 1;
 rule.tz = 'Etc/UTC';
 
-schedule.scheduleJob(rule, ()=>{
-    bot.sendMessage(testChat,`${array[counter]}${text}`)
+schedule.scheduleJob(rule, async()=>{
+    let current = await getFromDb()
+    if(current.data.data.date === gdate.getDate()){
+        return
+    }
+    const currentIndex = array.indexOf(current.data.data.name)
+    const nextIndex = (currentIndex + 1) % array.length
+    const currentName = array[nextIndex]
+    bot.sendMessage(testChat,`${currentName}${text}`)
+    putInDb(currentName,gdate.getDate())
 });
 
 
+
+
+async function getFromDb(){
+    return await axios({
+        method: 'get',
+        url: 'https://2a23c5d4-5735-4bb9-8398-abf11c30b867-europe-west1.apps.astra.datastax.com/api/rest/v2/namespaces/bot/collections/bot/bot',
+        headers: {
+            "accept": "application/json",
+            "X-Cassandra-Token": "AstraCS:rflmYZmoziplMovmyltPBuSf:bef5a0be80e89aac5aef5da437eef04ba7fcf06965f7c4f20685dae40b3171ab"
+        }
+      })
+}
+
+async function putInDb(name,date){
+    return await axios({
+        method: 'PUT',
+        url: 'https://2a23c5d4-5735-4bb9-8398-abf11c30b867-europe-west1.apps.astra.datastax.com/api/rest/v2/namespaces/bot/collections/bot/bot',
+        headers: {
+            "accept": "application/json",
+            "X-Cassandra-Token": "AstraCS:rflmYZmoziplMovmyltPBuSf:bef5a0be80e89aac5aef5da437eef04ba7fcf06965f7c4f20685dae40b3171ab",
+            "Content-Type": "application/json"
+        },
+        data:{
+            "name":name,
+            "date":date
+        }
+      })
+}
 
 app.listen(PORT,()=>{})
